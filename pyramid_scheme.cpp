6848f0ee09cb03906144d7b5671b081ad3751b07
Sofia Neves <at.work.sneves@gmail.com>
2022-04-06 16:34:01 +0100

    Input reader completed
    - Completed input reader

--- pyramid_scheme.cpp
+++ pyramid_scheme.cpp
@@ -1,72 +1,105 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> vector_, cost;
-vector<vector<int>> connections;
-char a;
+vector<int> vector_, cost, marker;
+vector<pair<int, vector<int>>> connections;
+
+string input;
+stringstream ss;
+
+void pyramid_scheme(int node)
+{
+    if (connections[node].second.size() == 0)
+    {
+        if (marker[connections[node].first] == 0)
+        {
+            marker[node] = 1;
+        }
+        return;
+    }
+
+    if (marker[connections[node].first] == 0)
+    {
+        marker[node] = 1;
+        for (unsigned long int i = 0; i < connections[node].second.size(); ++i)
+        {
+            pyramid_scheme(connections[node].second[i]);
+        }
+        marker[node] = 0;
+    }
+    else
+    {
+        marker[node] = 1;
+        for (unsigned long int i = 0; i < connections[node].second.size(); ++i)
+        {
+            pyramid_scheme(connections[node].second[i]);
+        }
+
+        marker[node] = 0;
+        for (unsigned long int i = 0; i < connections[node].second.size(); ++i)
+        {
+            pyramid_scheme(connections[node].second[i]);
+        }
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    int num, child;
-
-    connections = vector<vector<int>>(100001, vector<int>());
+    connections = vector<pair<int, vector<int>>>(100001, make_pair(-1, vector<int>()));
     cost = vector<int>(100001, 0);
-    int best = 0;
-    while (cin >> num)
+    marker = vector<int>(100001, 0);
+
+    string line;
+    int number, num, best = 0;
+    while (getline(cin, line))
     {
+        stringstream X(line);
+
+        X >> num;
+
         if (num > best)
         {
             best = num;
         }
-        child = 0;
         vector_ = vector<int>();
 
         if (num != -1)
         {
-
-            while ((a = cin.get()) != '\n')
+            for (int i = 0; X >> number; ++i)
             {
-                cout << "a: " << a << endl;
-                if (a != ' ')
+                vector_.push_back(number);
+            }
+
+            for (unsigned long int i = 0; i < vector_.size(); ++i)
+            {
+
+                if (i == vector_.size() - 1)
                 {
-                    child = child * 10 + int(a - '0');
-                    cout << "child " << child << endl;
+                    cost[num] = vector_[vector_.size() - 1];
+                    if (i == 0)
+                    {
+                        connections[vector_[i]].first = num;
+                    }
                 }
                 else
                 {
-                    cout << "else" << endl;
-                    vector_.push_back(child);
-                    child = 0;
-                }
-            }
-
-            // Process vector
-            for (unsigned long int i = 0; i < vector_.size(); ++i)
-            {
-                if (i == vector_.size() - 1)
-                {
-                    cost[vector_[0]] = vector_[vector_.size() - 1];
-                }
-                else if (i != 0)
-                {
-                    connections[vector_[0]].push_back(vector_[i]);
-                    connections[vector_[i]].push_back(vector_[0]);
+                    connections[vector_[i]].first = num;
+                    (connections[num].second).push_back(vector_[i]);
                 }
             }
         }
         else
         {
+            /*
             cout << "best: " << best << endl;
             // print connections
             for (int i = 0; i < best + 1; ++i)
             {
-                for (unsigned long int j = 0; j < connections[i].size(); ++j)
+                for (unsigned long int j = 0; j < connections[i].second.size(); ++j)
                 {
-
-                    cout << connections[i][j] << " ";
+                    cout << "connections[" << i << "][" << j << "] = FIRST: " << connections[i].first << "\tSECOND: " << connections[i].second[j] << " ";
                 }
                 cout << endl;
             }
@@ -75,13 +108,16 @@ int main()
             // print cost
             for (int i = 0; i < best + 1; ++i)
             {
-                cout << "i: " << i << " cost:" << cost[i] << " ";
+                cout << "i: " << i << " cost:" << cost[i] << " " << endl;
             }
 
-            connections = vector<vector<int>>();
-            cost = vector<int>();
+            cost = vector<int>();*/
+
+            connections = vector<pair<int, vector<int>>>(100001, make_pair(-1, vector<int>()));
+            cost = vector<int>(100001, 0);
+            best = 0;
+            marker = vector<int>(100001, 0);
         }
     }
-
     return 0;
 }
